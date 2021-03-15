af9b8dd7d69d3c6909b0bdd9011588d5b0f73d86
Jan0660 <jan0660@tutanota.com>
2021-03-15 17:00:35 +0100

    move CommandHandling code

---
+++ RevoltBot/CommandHandling/CommandInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using RevoltApi;
+using RevoltBot.Attributes;
+
+namespace RevoltBot
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class CommandInfo
+    {
+        public ModuleInfo Module;
+        public string[] Aliases;
+        public MethodInfo Method;
+        public PreconditionAttribute[] Preconditions;
+        public string Summary;
+        private string DebuggerDisplay
+        {
+            get
+            {
+                return string.Format("Command: {0}", Aliases.First());
+            }
+        }
+
+        public async Task ExecuteAsync(Message message, string args)
+        {
+            var module = Activator.CreateInstance(Method.DeclaringType!) as ModuleBase;
+            module!.Message = message;
+            module.Args = args;
+            var val = Method.Invoke(module, null);
+            if (val is Task task)
+                await task;
+        }
+    }
+}
---
+++ RevoltBot/CommandHandling/ModuleBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using RevoltApi;
+
+namespace RevoltBot
+{
+    public class ModuleBase
+    {
+        public Message Message;
+        public string Args;
+
+        public Task<Message> ReplyAsync(string content)
+            => Message.Channel.SendMessageAsync(content);
+    }
+}

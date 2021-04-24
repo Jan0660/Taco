b012fc310248f04ef1b1fed6daaa52b11b9d686e
Jan0660 <jan0660@tutanota.com>
2021-04-24 21:07:03 +0200

    implement suicide

--- Taco/Modules/ImageGenCommands.cs
+++ Taco/Modules/ImageGenCommands.cs
@@ -19,36 +19,25 @@ namespace RevoltBot.Modules
         [Command("jesusCarry")]
         public Task JesusCarry()
             => TemplateSend("JesusCarry.png", Message.AuthorId, new Point(162, 182));
+        [Command("suicide")]
+        public Task Suicide()
+            => TemplateSend("Suicide.png", Message.AuthorId, new Point(36, 103), size: 64);
 
-        public async Task<Message> TemplateSend(string template, string userId, Point location, bool rounded = true)
+        public async Task<Message> TemplateSend(string template, string userId, Point location, bool rounded = true, int size = 128)
         {
             var httpClient = new HttpClient();
-            var pfp = new Bitmap(await httpClient.GetStreamAsync($"{Message.Client.ApiUrl}/users/{userId}/avatar"));
+            var pfp = new Bitmap(await httpClient.GetStreamAsync($"{Message.Client.ApiUrl}/users/{userId}/avatar?size={size}"));
+            if (pfp.Height != size)
+            {
+                pfp = pfp.Resize(new Size(size, size), ImageFormat.Png);
+            }
             if (rounded)
-                pfp = RoundCorners(pfp, (int)(BaseRounding * ((double)pfp.Height / (double)BaseResolution)));
+                pfp = ImageGen.RoundCorners(pfp, (int)(BaseRounding * ((double)pfp.Height / (double)BaseResolution)));
             var img = ImageGen.Template(pfp, template, location);
             var stream = new MemoryStream();
             img.Save(stream, ImageFormat.Png);
             stream.Position = 0;
             return await Message.Channel.SendFileAsync("HH", "bruowh.png", stream.GetBuffer());
-        }
-        
-        public static Bitmap RoundCorners(Image image, int cornerRadius)
-        {
-            cornerRadius *= 2;
-            Bitmap roundedImage = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppArgb);
-            GraphicsPath gp = new GraphicsPath();
-            gp.AddArc(-1, -1, cornerRadius, cornerRadius, 180, 90);
-            gp.AddArc(0 + roundedImage.Width - cornerRadius, -1, cornerRadius, cornerRadius, 270, 90);
-            gp.AddArc(0 + roundedImage.Width - cornerRadius, 0 + roundedImage.Height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
-            gp.AddArc(-1, 0 + roundedImage.Height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
-            using (Graphics g = Graphics.FromImage(roundedImage))
-            {
-                g.SmoothingMode = SmoothingMode.HighQuality;
-                g.SetClip(gp);
-                g.DrawImage(image, Point.Empty);
-            }
-            return roundedImage;
         }
     }
 }
--- Taco/Util/ImageGen.cs
+++ Taco/Util/ImageGen.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 
 namespace RevoltBot.Util
@@ -14,6 +15,62 @@ namespace RevoltBot.Util
             gr.DrawImage(src, new Rectangle(0, 0, bmp.Width, bmp.Height));
             gr.DrawImage(pfp, point);
             return bmp;
+        }
+
+        //stolen from https://stackoverflow.com/a/24979829
+        public static Bitmap Resize(this Bitmap imgPhoto, Size objSize, ImageFormat enuType)
+        {
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+
+            int destX = 0;
+            int destY = 0;
+            int destWidth = objSize.Width;
+            int destHeight = objSize.Height;
+
+            Bitmap bmPhoto;
+            if (enuType == ImageFormat.Png)
+                bmPhoto = new Bitmap(destWidth, destHeight, PixelFormat.Format32bppArgb);
+            else if (enuType == ImageFormat.Gif)
+                bmPhoto = new Bitmap(destWidth, destHeight); //PixelFormat.Format8bppIndexed should be the right value for a GIF, but will throw an error with some GIF images so it's not safe to specify.
+            else
+                bmPhoto = new Bitmap(destWidth, destHeight, PixelFormat.Format24bppRgb);
+
+            //For some reason the resolution properties will be 96, even when the source image is different, so this matching does not appear to be reliable.
+            //bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            //If you want to override the default 96dpi resolution do it here
+            //bmPhoto.SetResolution(72, 72);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+        public static Bitmap RoundCorners(Image image, int cornerRadius)
+        {
+            cornerRadius *= 2;
+            Bitmap roundedImage = new Bitmap(image.Width, image.Height, PixelFormat.Format32bppArgb);
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddArc(-1, -1, cornerRadius, cornerRadius, 180, 90);
+            gp.AddArc(0 + roundedImage.Width - cornerRadius, -1, cornerRadius, cornerRadius, 270, 90);
+            gp.AddArc(0 + roundedImage.Width - cornerRadius, 0 + roundedImage.Height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
+            gp.AddArc(-1, 0 + roundedImage.Height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
+            using (Graphics g = Graphics.FromImage(roundedImage))
+            {
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.SetClip(gp);
+                g.DrawImage(image, Point.Empty);
+            }
+            return roundedImage;
         }
     }
 }

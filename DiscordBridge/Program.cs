d345eb9bd5552aae94e2a03acc473e31770dc14d
Jan0660 <jan0660@tutanota.com>
2021-05-14 14:46:41 +0200

    DiscordBridge/remove owo

--- DiscordBridge/Program.cs
+++ DiscordBridge/Program.cs
@@ -11,7 +11,6 @@ using Discord.Webhook;
 using Discord.WebSocket;
 using Log73;
 using Newtonsoft.Json;
-using Owoify;
 using RestSharp;
 using RevoltApi;
 using RevoltApi.Channels;
@@ -89,7 +88,7 @@ namespace DiscordBridge
                                     $"https://autumn.revolt.chat/attachments/{message.Attachment._id}/{HttpUtility.UrlEncode(message.Attachment.Filename)}"
                             }.Build()
                         };
-                    var msg = await discord.SendMessageAsync(Owoifier.Owoify(message.Content.ReplaceRevoltMentions(), Owoifier.OwoifyLevel.Uwu),
+                    var msg = await discord.SendMessageAsync(message.Content.ReplaceRevoltMentions(),
                         username: message.Author.Username,
                         avatarUrl: message.Author.Avatar == null ? message.Author.DefaultAvatarUrl : message.Author.AvatarUrl + "?size=256", allowedMentions: new(), embeds: embeds);
                     RevoltDiscordMessages.Add(message._id, msg);
